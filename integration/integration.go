4673a9ce477a0e1a7d89811c4c8eae1ff00f5a35
Emanuel Turis <hello@emanuelturis.com>
2024-01-23 22:25:41 +0000

    get integration tags function with app interface as arg

--- integration/integration.go
+++ integration/integration.go
@@ -1,6 +1,10 @@
 package integration
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 type Tag struct {
 	ID   string `json:"id"`
@@ -14,6 +18,29 @@ type App interface {
 	// Tags
 	GetTagsPath() string
 	UnmarshalTags(body []byte) ([]Tag, error)
+}
+
+func GetIntegrationTags(app App) ([]Tag, error) {
+	req, err := NewRequest("GET", app.URL())
+	if err != nil {
+		return []Tag{}, err
+	}
+	app.Authenticate(req)
+	req.URL.Path = fmt.Sprintf("%s/%s", req.URL.Path, app.GetTagsPath())
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return []Tag{}, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []Tag{}, err
+	}
+	tags, err := app.UnmarshalTags(body)
+	if err != nil {
+		return []Tag{}, err
+	}
+	return tags, nil
 }
 
 func NewRequest(method, url string) (*http.Request, error) {
