f364ef861b83ed3acfd5bc7a891f3200590e4a6e
Emanuel Turis <hello@emanuelturis.com>
2024-01-23 22:26:09 +0000

    get tags by app slug route

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/webdevfuel/go-saas-api-third-party-integration/integration"
 )
 
 func main() {
@@ -13,5 +16,29 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
 		w.Write([]byte("Hello, world!"))
 	})
+	r.Get("/tags/{app}", func(w http.ResponseWriter, r *http.Request) {
+		app := chi.URLParam(r, "app")
+		if app == "activecampaign" {
+			activeCampaignIntegration := integration.NewActiveCampaignIntegration()
+			tags, err := integration.GetIntegrationTags(activeCampaignIntegration)
+			if err != nil {
+				log.Println(err)
+			}
+			j, _ := json.Marshal(tags)
+			w.Write(j)
+			return
+		}
+		if app == "convertkit" {
+			convertKitIntegration := integration.NewConvertKitIntegration()
+			tags, err := integration.GetIntegrationTags(convertKitIntegration)
+			if err != nil {
+				log.Println(err)
+			}
+			j, _ := json.Marshal(tags)
+			w.Write(j)
+			return
+		}
+	})
+
 	http.ListenAndServe("localhost:8080", r)
 }
