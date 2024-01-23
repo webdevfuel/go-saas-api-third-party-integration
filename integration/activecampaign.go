24dafba0aaa01d370581aad77c97582161a51027
Emanuel Turis <hello@emanuelturis.com>
2024-01-23 22:24:05 +0000

    implement app interface methods on activecampaignintegration

--- integration/activecampaign.go
+++ integration/activecampaign.go
@@ -1,11 +1,47 @@
 package integration
 
-import "os"
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
+type ActiveCampaignTags struct {
+	Tags []struct {
+		ID  string `json:"id"`
+		Tag string `json:"tag"`
+	} `json:"tags"`
+}
 
 type ActiveCampaignIntegration struct {
 	APIKey   string
 	APIURL   string
 	TagsPath string
+}
+
+func (integration ActiveCampaignIntegration) URL() string {
+	return integration.APIURL
+}
+
+func (integration ActiveCampaignIntegration) Authenticate(request *http.Request) {
+	request.Header.Add("Api-Token", integration.APIKey)
+}
+
+func (integration ActiveCampaignIntegration) GetTagsPath() string {
+	return integration.TagsPath
+}
+
+func (integration ActiveCampaignIntegration) UnmarshalTags(data []byte) ([]Tag, error) {
+	var activeCampaignTags ActiveCampaignTags
+	err := json.Unmarshal(data, &activeCampaignTags)
+	if err != nil {
+		return []Tag{}, err
+	}
+	var tags []Tag
+	for _, tag := range activeCampaignTags.Tags {
+		tags = append(tags, Tag{ID: tag.ID, Name: tag.Tag})
+	}
+	return tags, nil
 }
 
 func NewActiveCampaignIntegration() *ActiveCampaignIntegration {
